af2926504f20194d2170b357050efb31cde76f74
alfonsoj-entwickler <alfonsoj.entwickler@gmail.com>
2024-02-27 19:06:19 +0100

    [TASK] update component

--- src/todos/components/TodoItem.tsx
+++ src/todos/components/TodoItem.tsx
@@ -1,21 +1,28 @@
 import { Todo } from "@prisma/client";
+import styles from "./TodoItem.module.scss";
+import { IoCheckboxOutline, IoSquareOutline } from "react-icons/io5";
 
 interface Props {
   todo: Todo;
 }
 export const TodoItem = ({ todo }: Props) => {
   return (
-    <div className="flex flex-col gap-4 p-6 rounded-xl bg-green-600 text-white">
-      <div className="text-base font-light">
-        <span>{todo.id}</span>
+    <div className={todo?.complete ? styles.todoDone : styles.todoPending}>
+      <div className="flex flex-col sm:flex-row justify-start items-center gap-4">
+        <div
+          className={`
+          flex p-2 roundend-md cursor-pointer hover:bg-opacity-60 
+          ${todo?.complete ? "bg-blue-100" : "bg-red-100"}
+        `}
+        >
+          {todo?.complete ? (
+            <IoCheckboxOutline size={30} />
+          ) : (
+            <IoSquareOutline size={30} />
+          )}
+        </div>
+        <div className="text-center sm:text-left">{todo?.description}</div>
       </div>
-      <div className="text-xl font-semibold">
-        <span>{todo.description}</span>
-      </div>
-      <div className="text-base font-light">
-        <span>{todo.updateAt}</span>
-      </div>
-    </>
-  )
-  ;
+    </div>
+  );
 };
