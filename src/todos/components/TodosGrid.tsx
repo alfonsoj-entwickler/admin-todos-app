d151b76c29d1ad011cf102c4c08a16ee6a5505a4
alfonsoj-entwickler <alfonsoj.entwickler@gmail.com>
2024-02-29 19:32:56 +0100

    [TASK] component grid for todos

--- src/todos/components/TodosGrid.tsx
+++ src/todos/components/TodosGrid.tsx
@@ -6,14 +6,12 @@ interface Props {
 }
 
 export const TodosGrid = ({ todos = [] }: Props) => {
-
+  console.log(todos);
   return (
     <div className="grid grid-cols-1 sm:grid-cols-3 gap-2">
-        {
-            todos.map(todo => (
-                <TodoItem key={todo.id} {...todo} />
-            ))
-        }
+      {todos.map((todo) => (
+        <TodoItem key={todo.id} todo={todo} />
+      ))}
     </div>
   );
 };
