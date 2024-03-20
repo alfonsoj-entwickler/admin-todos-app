0da15c94b3ca1304453468ccad77c7726b9d511a
alfonsoj-entwickler <alfonsoj.entwickler@gmail.com>
2024-03-20 18:44:23 +0100

    [TASK] add optimistic of react

--- src/todos/components/TodoItem.tsx
+++ src/todos/components/TodoItem.tsx
@@ -1,31 +1,58 @@
-'use client';
-
+"use client";
+import { useOptimistic, startTransition } from "react";
 import { Todo } from "@prisma/client";
 import styles from "./TodoItem.module.scss";
 import { IoCheckboxOutline, IoSquareOutline } from "react-icons/io5";
 
 interface Props {
   todo: Todo;
-  toggleTodo: ( id: string, cpmplete: boolean ) => Promise<Todo|void>
+  toggleTodo: (id: string, cpmplete: boolean) => Promise<Todo | void>;
 }
 export const TodoItem = ({ todo, toggleTodo }: Props) => {
+  const [todoOptimistic, setTodoOptimistic] = useOptimistic(
+    todo,
+    (state, newCompleteValue: boolean) => ({
+      ...state,
+      complete: newCompleteValue,
+    })
+  );
+
+  const onToggleTodo = async()=> {
+    try {
+      startTransition( ()=> {
+        setTodoOptimistic( !todoOptimistic.complete )
+      });
+      
+      await toggleTodo( todoOptimistic.id, !todoOptimistic.complete )
+    } catch (error) {
+      startTransition( ()=> {
+        setTodoOptimistic( !todoOptimistic.complete )
+      });
+    }
+  }
+
   return (
-    <div className={todo?.complete ? styles.todoDone : styles.todoPending}>
+    <div
+      className={todoOptimistic.complete ? styles.todoDone : styles.todoPending}
+    >
       <div className="flex flex-col sm:flex-row justify-start items-center gap-4">
         <div
-          onClick={ () => toggleTodo( todo.id, !todo.complete ) }
+          // onClick={() => toggleTodo(todo.id, !todo.complete)}
+          onClick={onToggleTodo}
           className={`
           flex p-2 roundend-md cursor-pointer hover:bg-opacity-60 
-          ${todo?.complete ? "bg-blue-100" : "bg-red-100"}
+          ${todoOptimistic.complete ? "bg-blue-100" : "bg-red-100"}
         `}
         >
-          {todo?.complete ? (
+          {todoOptimistic.complete ? (
             <IoCheckboxOutline size={30} />
           ) : (
             <IoSquareOutline size={30} />
           )}
         </div>
-        <div className="text-center sm:text-left">{todo?.description}</div>
+        <div className="text-center sm:text-left">
+          {todoOptimistic.description}
+        </div>
       </div>
     </div>
   );
