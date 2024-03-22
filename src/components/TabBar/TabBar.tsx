04e9e421bc9fa2925c569c520e986206d05ae6f6
alfonsoj-entwickler <alfonsoj.entwickler@gmail.com>
2024-03-22 19:22:25 +0100

    [TASK] create component tabbar in header

---
+++ src/components/TabBar/TabBar.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { setCookie } from "cookies-next";
+import { useState } from "react";
+
+interface Props {
+  currentTab?: number;
+  tabOptions?: number[];
+}
+
+export const TabBar = ({
+  tabOptions = [1, 2, 3, 4],
+  currentTab = 1,
+}: Props) => {
+  const [selected, setSeleted] = useState(currentTab);
+
+  const onTabSelected = (tab: number) => {
+    setSeleted(tab);
+    setCookie("selectedTab", tab.toString());
+  };
+  return (
+    <div
+      className={`
+        grid grid-cols-5 w-full space-x-2 rounded-xl bg-gray-200 p-2
+    `}
+    >
+      {tabOptions.map((tab) => (
+        <div key={`tab-${tab}`}>
+          <input
+            type="radio"
+            checked={selected === tab}
+            onChange={() => {}}
+            id={tab.toString()}
+            className="peer hidden"
+          />
+          <label
+            onClick={() => onTabSelected(tab)}
+            className="block cursor-pointer select-none rounded-xl p-2 text-center peer-checked:bg-blue-500 peer-checked:font-bold peer-checked:text-white transition"
+          >
+            {tab}
+          </label>
+        </div>
+      ))}
+    </div>
+  );
+};
