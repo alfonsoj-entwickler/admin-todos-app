21367fd6dc9497e5f20c42e288a68691ddc9346a
alfonsoj-entwickler <alfonsoj.entwickler@gmail.com>
2024-05-21 18:33:11 +0200

    [TASK] updated component in dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,7 +2,9 @@ import WidgetItem from "../../components/WidgetItem";
 export default function Dashboard() {
   return (
     <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
-      <WidgetItem />
+      <WidgetItem title={'Dashboard'}>
+        <div></div>
+      </WidgetItem>
     </div>
   );
 }
