b9f183dc8426bae0149ed6408abbe2465674fce7
alfonsoj-entwickler <alfonsoj.entwickler@gmail.com>
2024-05-30 21:39:55 +0200

    [TASK] redirect home to dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
- 
+import { redirect } from "next/navigation";
 
 export default function Home() {
+  redirect("/dashboard");
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-6xl font-bold underline">Admin ToDo</h1>
     </main>
-  )
+  );
 }
